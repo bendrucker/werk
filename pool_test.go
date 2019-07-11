d8ce068c1528ae0f10fec797e812c9a06a13ac21
Ben Drucker <bvdrucker@gmail.com>
2019-07-10 18:21:25 -0700

    remove WorkFunc wrapper from pool tests

--- pool_test.go
+++ pool_test.go
@@ -13,11 +13,11 @@ func TestPool(t *testing.T) {
 
 	assert.Equal(t, pool.Available(), 10)
 
-	pool.Do(Work{"hello", 0}, WorkFunc(func(_ context.Context, v interface{}) error {
+	pool.Do(Work{"hello", 0}, func(_ context.Context, v interface{}) error {
 		assert.Equal(t, "hello", v.(string))
 		assert.Equal(t, 9, pool.Available())
 		return nil
-	}))
+	})
 
 	assert.Equal(t, pool.Available(), 10)
 }
@@ -28,11 +28,11 @@ func TestPoolTimeout(t *testing.T) {
 	timeout := time.Duration(10) * time.Millisecond
 	errors := make(chan error, 1)
 
-	pool.Do(Work{"hello", timeout}, WorkFunc(func(ctx context.Context, v interface{}) error {
+	pool.Do(Work{"hello", timeout}, func(ctx context.Context, v interface{}) error {
 		<-ctx.Done()
 		errors <- ctx.Err()
 		return nil
-	}))
+	})
 
 	err := <-errors
 	assert.Equal(t, context.DeadlineExceeded, err)
